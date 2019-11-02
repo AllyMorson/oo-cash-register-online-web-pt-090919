e3562fad7e6f611d6aaba90b658b85932327ebbb
Ally Morson <ally.morson@gmail.com>
2019-11-02 01:14:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,14 +3,14 @@ class CashRegister
 
   attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(discount=0)
-    @total = 0
+  def initialize(discount=0) #optionally takes an employee discount which is why we have set to 0
+    @total = 0 #sets an instance variable @total on initialization to zero
     @discount = discount
     @items = []
   end
 
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
+  def add_item(title, amount, quantity=1) #accepts a title and a price , an optional quantity and increases the total
+    self.total += amount * quantity #doesn't forget about the previous total 
     quantity.times do
       items << title
     end
@@ -22,11 +22,11 @@ class CashRegister
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
       "After the discount, the total comes to $#{self.total}."
     else
-      "There is no discount to apply."
+      "There is no discount to apply." #returns a string error message that there is no discount to apply
     end
   end
 
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total = self.total - self.last_transaction # subtracts the last item from the total
   end
 end
